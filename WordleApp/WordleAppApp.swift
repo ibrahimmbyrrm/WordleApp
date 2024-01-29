6958755f77b07b6ebe74e21265578e982808b830
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2024-01-30 02:20:41 +0300

    Initial Commit

---
+++ WordleApp/WordleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleAppApp.swift
+//  WordleApp
+//
+//  Created by İbrahim Bayram on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
